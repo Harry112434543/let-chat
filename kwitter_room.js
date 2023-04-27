49292e7491c4d8bedad0d3d709be358065475d66
Harry112434543 <112751756+Harry112434543@users.noreply.github.com>
2023-04-27 18:56:23 +0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDlHIvSHDlO5htGWw6kIQypq-_B8Xsh9w0",
+    authDomain: "let-chat-app-c2cdc.firebaseapp.com",
+    projectId: "let-chat-app-c2cdc",
+    storageBucket: "let-chat-app-c2cdc.appspot.com",
+    messagingSenderId: "915399465680",
+    appId: "1:915399465680:web:6d907cf1d87860953f9cd5"
+  };
+  
+  // Initialize Firebase
+  firebase.database.initializeApp(firebaseConfig);
+ 
